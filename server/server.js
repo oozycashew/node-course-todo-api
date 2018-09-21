dd1d7c55bbd4e84b299e1d0661b7ade1e8a64178
Ray Go <raymondjgo@gmail.com>
2018-09-20 21:34:46 -0700

    Fixed typo for app listen output

--- server/server.js
+++ server/server.js
@@ -49,7 +49,7 @@ app.get('/todos/:id', (req, res) => {
 
 
 app.listen(port, () => {
-  console.log(`Started on port {$port}`);
+  console.log(`Started on port ${port}`);
 });
 
 module.exports = {app};
